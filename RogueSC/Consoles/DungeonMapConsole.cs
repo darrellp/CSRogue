326ea67415964b0c3eade24d2068e4f01d74e7a3
Darrell Plank <jar_czar@msn.com>
2016-08-28 07:56:43 -0700

    Modified the externally added code and it's very quirky commenting style to fit in with my code better.

--- RogueSC/Consoles/DungeonMapConsole.cs
+++ RogueSC/Consoles/DungeonMapConsole.cs
@@ -69,13 +69,13 @@ namespace RogueSC.Consoles
         public DungeonMapConsole(int viewWidth, int viewHeight, int mapWidth, int mapHeight, Font.FontSizes fontSize = Font.FontSizes.One) 
             : base(mapWidth, mapHeight)
         {
-            FontMaster fontMaster = Engine.LoadFont("Cheepicus12.font");
-            Font font = fontMaster.GetFont(fontSize);
+            var fontMaster = Engine.LoadFont("Cheepicus12.font");
+            var font = fontMaster.GetFont(fontSize);
             TextSurface.Font = font;
             var mult = SizeMultipliers[fontSize];
             TextSurface.RenderArea = new Rectangle(0, 0, (int)(viewWidth * 2 / (3 * mult)), (int)(viewHeight * 3 / (2 * mult)) - 1);
 
-            AnimatedTextSurface playerAnimation = new AnimatedTextSurface("default", 1, 1, font);
+            var playerAnimation = new AnimatedTextSurface("default", 1, 1, font);
             playerAnimation.CreateFrame();
             playerAnimation.CurrentFrame[0].Foreground = Color.Orange;
             playerAnimation.CurrentFrame[0].GlyphIndex = 1;//'@';
