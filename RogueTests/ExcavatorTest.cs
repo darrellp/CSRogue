95334239d716355d93a04916a36cee5e21852e9b
Darrell Plank <jar_czar@msn.com>
2016-09-01 07:20:09 -0700

    Moved the TestFactory into it's own little class to make it an easier example to follow.

--- RogueTests/ExcavatorTest.cs
+++ RogueTests/ExcavatorTest.cs
@@ -7,6 +7,7 @@ using System.IO;
 using System.Runtime.Remoting.Messaging;
 using CSRogue.Item_Handling;
 using CSRogue.Map_Generation;
+using static RogueTests.TestFactory;
 
 namespace RogueTests
 {
@@ -17,7 +18,7 @@ namespace RogueTests
 	///to contain all ExcavatorTest Unit Tests
 	///</summary>
 	[TestClass()]
-	public class ExcavatorTest
+	public partial class ExcavatorTest
 	{
 	    /// <summary>
 		///Gets or sets the test context which provides
@@ -85,52 +86,7 @@ namespace RogueTests
             Assert.AreEqual(terrain, csRogueMap.Terrain(iCol, iRow));
         }
 
-        static Guid personId = new Guid("0D583F58-FA20-4292-A272-37919917644A");
-        static Guid ratId = new Guid("1BA9B9C4-6133-4CD3-92A6-233F0F26CBC0");
-
-        class Person : IItem
-	    {
-	        public Guid ItemTypeId { get; } = personId;
-	        public MapCoordinates Location { get; set; }
-	        public char Ch { get; set; } = '@';
-	    }
-
-        class Rat : IItem
-        {
-            public Guid ItemTypeId { get; } = ratId;
-            public MapCoordinates Location { get; set; }
-            public char Ch { get; set; } = 'r';
-        }
-
-	    static Dictionary<Guid, Func<IItem>> GuidToCreator = new Dictionary<Guid, Func<IItem>>()
-        {
-	        {ratId, () => new Rat()},
-            {personId, () => new Person() }
-	    };
-
-        class TestFactory : IItemFactory
-	    {
-	        public Dictionary<Guid, ItemInfo> InfoFromId { get; }
-	        public IItem Create(Guid id, Level level)
-	        {
-	            return GuidToCreator[id]();
-	        }
-
-	        public TestFactory()
-	        {
-                const string input = @"
-//type										ch	name		weight	value	description
-0D583F58-FA20-4292-A272-37919917644A		@	Player		.		.		The Player
-1BA9B9C4-6133-4CD3-92A6-233F0F26CBC0		r	Rat			.		.		A vile little sewer rat.
-																			These rodents seem to be everywhere!
-";
-
-                TextReader reader = new StringReader(input);
-                InfoFromId = ReadItemData.GetData(reader);
-            }
-        }
-
-        /// <summary>
+	    /// <summary>
         ///A test for Excavate
         ///</summary>
         [TestMethod()]
@@ -146,8 +102,8 @@ namespace RogueTests
             FileExcavator excavator = new FileExcavator(stream, new TestFactory());
             CsRogueMap csRogueMap = new CsRogueMap();
             excavator.Excavate(csRogueMap);
-            CheckLocation(csRogueMap, 0, 0, TerrainType.Floor, personId);
-            CheckLocation(csRogueMap, 1, 0, TerrainType.Floor, ratId);
+            CheckLocation(csRogueMap, 0, 0, TerrainType.Floor, PersonId);
+            CheckLocation(csRogueMap, 1, 0, TerrainType.Floor, RatId);
             CheckLocation(csRogueMap, 2, 0, TerrainType.HorizontalWall);
             CheckLocation(csRogueMap, 3, 0, TerrainType.Floor);
             CheckLocation(csRogueMap, 14, 0, TerrainType.Floor);
---
+++ RogueTests/TestFactory.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using CSRogue.Item_Handling;
+using CSRogue.Map_Generation;
+
+namespace RogueTests
+{
+    public class TestFactory : IItemFactory
+    {
+        public static Guid PersonId = new Guid("0D583F58-FA20-4292-A272-37919917644A");
+        public static Guid RatId = new Guid("1BA9B9C4-6133-4CD3-92A6-233F0F26CBC0");
+
+        class Person : IItem
+        {
+            public Guid ItemTypeId { get; } = PersonId;
+            public MapCoordinates Location { get; set; }
+            public char Ch { get; set; } = '@';
+        }
+
+        class Rat : IItem
+        {
+            public Guid ItemTypeId { get; } = RatId;
+            public MapCoordinates Location { get; set; }
+            public char Ch { get; set; } = 'r';
+        }
+
+        static readonly Dictionary<Guid, Func<IItem>> GuidToCreator = new Dictionary<Guid, Func<IItem>>()
+        {
+            {RatId, () => new Rat()},
+            {PersonId, () => new Person() }
+        };
+        public Dictionary<Guid, ItemInfo> InfoFromId { get; }
+        public IItem Create(Guid id, Level level)
+        {
+            return GuidToCreator[id]();
+        }
+
+        public TestFactory()
+        {
+            const string input = @"
+//type										ch	name		weight	value	description
+0D583F58-FA20-4292-A272-37919917644A		@	Player		.		.		The Player
+1BA9B9C4-6133-4CD3-92A6-233F0F26CBC0		r	Rat			.		.		A vile little sewer rat.
+																			These rodents seem to be everywhere!
+";
+
+            TextReader reader = new StringReader(input);
+            InfoFromId = ReadItemData.GetData(reader);
+        }
+    }
+}
