de9bcf36d87e7e481e1a2caadf255d607bd07a96
Darrell Plank <jar_czar@msn.com>
2016-09-09 09:18:00 -0700

    Added in Perlin Noise class

--- RogueTests/MovementCommandTest.cs
+++ RogueTests/MovementCommandTest.cs
@@ -1,13 +1,17 @@
-﻿using CSRogue.GameControl;
+﻿using System;
+using CSRogue.GameControl;
 using CSRogue.GameControl.Commands;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using CSRogue.Map_Generation;
+using CSRogue.RogueEventArgs;
 
 namespace RogueTests
 {
     [TestClass]
     public class MovementCommandTest
     {
+        private CreatureMoveEventArgs moveArgs;
+
         [TestMethod]
         public void TestMovement()
         {
@@ -21,8 +25,21 @@ namespace RogueTests
             var levelCmd = new NewLevelCommand(0, map);
             game.EnqueueAndProcess(levelCmd);
             var moveCmd = new MovementCommand(MoveDirection.MoveLowerRight);
+            game.HeroMoveEvent += GameOnHeroMoveEvent;
             game.EnqueueAndProcess(moveCmd);
             Assert.AreEqual(new MapCoordinates(1,1), map.Player.Location);
+            Assert.IsNotNull(moveArgs);
+            Assert.IsTrue(moveArgs.IsPlayer);
+            Assert.AreEqual(new MapCoordinates(0, 0), moveArgs.PreviousCreatureLocation);
+            Assert.AreEqual(new MapCoordinates(1, 1), moveArgs.CreatureDestination);
+            Assert.IsFalse(moveArgs.IsBlocked);
+            Assert.IsFalse(moveArgs.IsRunning);
+            Assert.IsFalse(moveArgs.IsFirstTimePlacement);
+        }
+
+        private void GameOnHeroMoveEvent(object sender, CreatureMoveEventArgs creatureMoveEventArgs)
+        {
+            moveArgs = creatureMoveEventArgs;
         }
     }
 }
