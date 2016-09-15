4d2bb4a4bf584faa460a6d928ccb181363c561ec
Darrell Plank <jar_czar@msn.com>
2016-09-15 05:36:26 -0700

    Wasn't using command arguments correctly in ToggleDoor command.

--- RogueSC/Commands/ToggleDoorCommand.cs
+++ RogueSC/Commands/ToggleDoorCommand.cs
@@ -1,21 +1,15 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using CSRogue.GameControl.Commands;
 using CSRogue.Interfaces;
 using CSRogue.Map_Generation;
-using CSRogue.Utilities;
-using Microsoft.Xna.Framework;
 using Game = CSRogue.GameControl.Game;
 
 namespace RogueSC.Commands
 {
     class ToggleDoorCommand : IRogueCommand
     {
-        private ICreature _agent;
-        private MapCoordinates _doorLocation;
+        private readonly ICreature _agent;
+        private readonly MapCoordinates _doorLocation;
 
         /// <summary> Event queue for all listeners interested in new level events. </summary>
         internal static event EventHandler<ToogleDoorEventArgs> ToogleDoorEvent;
@@ -34,15 +28,8 @@ namespace RogueSC.Commands
         public void Execute(Game game)
         {
             SCMap scMap = (SCMap) game.Map;
-
-            foreach (var doorLoc in scMap.Neighbors(scMap.Player.Location).Where(l => scMap[l].Terrain == TerrainType.Door))
-            {
-                if (scMap[doorLoc].Items.Count == 0)
-                {
-                    scMap[doorLoc].ToggleDoor();
-                    InvokeNewLevelEvent(game, new ToogleDoorEventArgs(scMap.Player, doorLoc));
-                }
-            }
+            scMap[_doorLocation].ToggleDoor();
+            InvokeNewLevelEvent(game, new ToogleDoorEventArgs(_agent, _doorLocation));
             var moveCmd = new MovePlayerCommand(new MapCoordinates());
             game.EnqueueAndProcess(moveCmd);
         }
