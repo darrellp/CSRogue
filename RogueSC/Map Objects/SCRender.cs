a0a311dcb4c557b17dcb3f5abbb89983c9cf5ab5
Darrell Plank <jar_czar@msn.com>
2016-09-16 13:59:13 -0700

    Showing the stairways

--- RogueSC/Map Objects/SCRender.cs
+++ RogueSC/Map Objects/SCRender.cs
@@ -31,6 +31,13 @@ namespace RogueSC.Map_Objects
             CloneOnApply = false
             };
 
+        internal static readonly CellAppearance WallAppearance = new CellAppearance(Color.Gray, Color.Blue, WallGlyph);
+        internal static readonly CellAppearance FloorAppearance = new CellAppearance(Color.Orange, Color.Transparent, FloorGlyph);
+		internal static readonly CellAppearance OffMapAppearance = new CellAppearance(Color.Black, Color.Black, FloorGlyph);
+		internal static readonly CellAppearance DoorAppearance = new CellAppearance(Color.Orange, Color.Transparent, DoorGlyph);
+        internal static readonly CellAppearance StairsDownAppearance = new CellAppearance(Color.Yellow, Color.Transparent, '<');
+        internal static readonly CellAppearance StairsUpAppearance = new CellAppearance(Color.Yellow, Color.Transparent, '>');
+
         internal static Dictionary<string, CellAppearance> ObjectNameToAppearance = new Dictionary<string, CellAppearance>()
         {
             {"wall", new CellAppearance(Color.Gray, Color.Blue, WallGlyph)},
@@ -41,25 +48,22 @@ namespace RogueSC.Map_Objects
 			{"openDoor", new CellAppearance(Color.Yellow, Color.Transparent, DoorGlyph) },
 			{"closedDoor", new CellAppearance(Color.Orange, Color.Transparent, DoorGlyph) },
             {"groundCover", new CellAppearance(Color.Green, Color.Transparent, '~') },
+            {"stairsDown", new CellAppearance(Color.Yellow, Color.Transparent, '>') },
+            {"stairsUp", new CellAppearance(Color.Yellow, Color.Transparent, '<') },
         };
 
-        internal static readonly CellAppearance WallAppearance = new CellAppearance(Color.Gray, Color.Blue, WallGlyph);
-        internal static readonly CellAppearance FloorAppearance = new CellAppearance(Color.Orange, Color.Transparent, FloorGlyph);
-		internal static readonly CellAppearance OffMapAppearance = new CellAppearance(Color.Black, Color.Black, FloorGlyph);
-		internal static readonly CellAppearance DoorAppearance = new CellAppearance(Color.Orange, Color.Transparent, DoorGlyph);
-
-		/// <summary>   Maps terrain types to appearance for that terrain. </summary>
-		/// Note that "Appearance" means a character, a foreground and a background color.  We display hidden and remembered
-		/// cells by using the same appearance and changing the effect on it with a Recolor effect which modifies the foreground
-		/// and background colors without changing the glyph.  So, in reality as things stand now with a static background we
-		/// just need to change effects on newly seen and newly unseen characters.
-		internal static readonly Dictionary<TerrainType, CellAppearance> MapTerrainToAppearance = new Dictionary
+        /// <summary>   Maps terrain types to appearance for that terrain. </summary>
+        /// Note that "Appearance" means a character, a foreground and a background color.  We display hidden and remembered
+        /// cells by using the same appearance and changing the effect on it with a Recolor effect which modifies the foreground
+        /// and background colors without changing the glyph.  So, in reality as things stand now with a static background we
+        /// just need to change effects on newly seen and newly unseen characters.
+        internal static readonly Dictionary<TerrainType, CellAppearance> MapTerrainToAppearance = new Dictionary
 		    <TerrainType, CellAppearance>()
 	    {
 		    {TerrainType.Floor, FloorAppearance},
 		    {TerrainType.Door, DoorAppearance},
-		    {TerrainType.StairsDown, FloorAppearance},
-		    {TerrainType.StairsUp, FloorAppearance},
+		    {TerrainType.StairsDown, StairsDownAppearance},
+		    {TerrainType.StairsUp, StairsUpAppearance},
 		    {TerrainType.Corner, WallAppearance},
 		    {TerrainType.HorizontalWall, WallAppearance},
 		    {TerrainType.VerticalWall, WallAppearance},
